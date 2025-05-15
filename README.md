0c60e0b860ee011e3eca3f873f7f7a45d8801e21
Jonathan Li <jonathanli668899@gmail.com>
2025-05-15 18:31:04 -0400

    face & eye better tuned for me

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # PyTorchStuff
 
-This has a lot of random stuff like opencv random stuff. The pytorch stuff is in the jupyter notebook files.
+This has a lot of random stuff like opencv random stuff. The pytorch stuff is in the jupyter notebook files (except pandas)
--- face.py
+++ face.py
@@ -5,23 +5,26 @@ cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
-
 while True:
     ret, frame = cap.read()
+
+    font = cv2.FONT_HERSHEY_COMPLEX
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = face_cascade.detectMultiScale(gray, 1.3, 5) #scale, minNeighbors  
 
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 5)
+        cv2.putText(frame, 'face', (x, y), font, 2, (235, 206, 135), 4, cv2.LINE_AA)
         roi_gray = gray[y:y+w, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
 
-        eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
+        eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor = 1.3, minNeighbors = 10, minSize = (60, 10), maxSize = (200, 100))
 
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 5)
+            cv2.putText(frame, 'eye', (x+ex, y+ey), font, 2, (0, 0, 0), 4, cv2.LINE_AA)
 
     cv2.imshow('frame', frame)
 
